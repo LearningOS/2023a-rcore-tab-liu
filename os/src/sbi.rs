cd26c97266be5ce3841cb576222f2d01f0cc431f
Tab Liu <dearhange@126.com>
2023-10-25 22:47:53 +0800

    update

--- os/src/sbi.rs
+++ os/src/sbi.rs
@@ -4,7 +4,7 @@ use core::arch::asm;
 
 const SBI_SET_TIMER: usize = 0;
 const SBI_CONSOLE_PUTCHAR: usize = 1;
-const SBI_SHUTDOWN: usize = 8;
+// const SBI_SHUTDOWN: usize = 0;
 
 /// general sbi call
 #[inline(always)]
@@ -23,6 +23,21 @@ fn sbi_call(which: usize, arg0: usize, arg1: usize, arg2: usize) -> usize {
     ret
 }
 
+#[inline(always)]
+fn sbi_call_new(eid: usize, fid: usize, arg0: usize, arg1: usize, arg2: usize) -> usize {
+    let mut ret;
+    unsafe {
+        asm!(
+            "ecall",
+            inlateout("a0") arg0 => _,
+            inlateout("a1") arg1 => ret,
+            in("a2") arg2,
+            in("a6") fid,
+            in("a7") eid,
+        );
+    }
+    ret
+}
 /// use sbi call to set timer
 pub fn set_timer(timer: usize) {
     sbi_call(SBI_SET_TIMER, timer, 0, 0);
@@ -35,6 +50,6 @@ pub fn console_putchar(c: usize) {
 
 /// use sbi call to shutdown the kernel
 pub fn shutdown() -> ! {
-    sbi_call(SBI_SHUTDOWN, 0, 0, 0);
+    sbi_call_new(0x53525354, 0, 0, 0, 0);
     panic!("It should shutdown!");
 }
